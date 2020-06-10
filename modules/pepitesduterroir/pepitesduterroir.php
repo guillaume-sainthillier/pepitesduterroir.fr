f9b39f6ad68826d183de9a9884afab5687197f4a
Guillaume Sainthillier <guillaume.sainthillier@gmail.com>
2020-06-10 15:40:40 +0200

    Added unit price

--- modules/pepitesduterroir/pepitesduterroir.php
+++ modules/pepitesduterroir/pepitesduterroir.php
@@ -25,41 +25,77 @@
 */
 
 if (!defined('_PS_VERSION_'))
-	exit;
+    exit;
 
 class PepitesDuTerroir extends Module
 {
-	public function __construct()
-	{
-		$this->name = 'pepitesduterroir';
-		$this->tab = 'administration';
-		$this->version = '1.0.0';
-		$this->author = 'Guillaume Sainthillier';
-		$this->need_instance = 0;
+    public function __construct()
+    {
+        $this->name = 'pepitesduterroir';
+        $this->tab = 'administration';
+        $this->version = '1.0.0';
+        $this->author = 'Guillaume Sainthillier';
+        $this->need_instance = 0;
 
-		parent::__construct();
+        parent::__construct();
 
-		$this->displayName = $this->trans('Les pépites du terroir', array(), 'Modules.PepitesDuTerroir.Admin');
-		$this->description = $this->trans('Configure les fonctionnalités sur mesures des pépites du terroir.', array(), 'Modules.PepitesDuTerroir.Admin');
-		$this->ps_versions_compliancy = array('min' => '1.7.1.0', 'max' => _PS_VERSION_);
-	}
-
-	public function install() {
-	    parent::install();
-        $this->registerHook('filterProductSearch');
+        $this->displayName = $this->trans('Les pépites du terroir', array(), 'Modules.PepitesDuTerroir.Admin');
+        $this->description = $this->trans('Configure les fonctionnalités sur mesures des pépites du terroir.', array(), 'Modules.PepitesDuTerroir.Admin');
+        $this->ps_versions_compliancy = array('min' => '1.7.1.0', 'max' => _PS_VERSION_);
     }
 
-    public function uninstall() {
-	    parent::uninstall();
+    public function install()
+    {
+        parent::install();
+        $this->registerHook('filterProductSearch');
+        $this->registerHook('displayProductPriceBlock');
+    }
+
+    public function uninstall()
+    {
+        parent::uninstall();
         $this->unregisterHook('filterProductSearch');
+        $this->unregisterHook('displayProductPriceBlock');
     }
 
     public function hookFilterProductSearch(array $params)
     {
-        foreach($params['searchVariables']['sort_orders'] as $key => $sort) {
-            if($sort['field'] === 'name') {
+        foreach ($params['searchVariables']['sort_orders'] as $key => $sort) {
+            if ($sort['field'] === 'name') {
                 unset($params['searchVariables']['sort_orders'][$key]);
             }
         }
+    }
+
+    public function hookDisplayProductPriceBlock(array $params)
+    {
+        if (!in_array($params['type'], ['unit_price', 'after_price'], true)) {
+            return;
+        }
+
+        $product = $params['product'];
+        if (false === stripos($product['name'], 'carton ')) {
+            return;
+        }
+
+        $price = $product['price_amount'];
+        $regularPrice = $product['regular_price_amount'];
+        $unitPrice = Context::getContext()->currentLocale->formatPrice($price / 6, 'EUR');
+        if ($product instanceof \PrestaShop\PrestaShop\Adapter\Presenter\Product\ProductListingLazyArray) {
+            //List product
+            return '<small class="w-100 mt-1 font-italic">Soit <span class="price product-price" >' . $unitPrice . '</span> par unité</small>';
+        }
+
+        $regularUnitPrice = Context::getContext()->currentLocale->formatPrice($regularPrice / 6, 'EUR');
+        $regularPriceDescription = '';
+        if($regularPrice !== $price) {
+            $regularPriceDescription = ' (au lieu de <span class="price product-price" >' . $regularUnitPrice . '</span>)';
+        }
+
+        return '
+            <div class="w-100 mt-2 product-prices-wrapper">
+            <small><small>Soit <span class="price product-price" >' . $unitPrice . '</span> par unité'.$regularPriceDescription.'</small></small>
+            </div>
+        ';
     }
 }
